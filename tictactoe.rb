2fd6e4deec94afa82539a06aac6388097dcaa9d4
Callum Walsh <makomirocket@gmail.com>
2019-10-06 06:50:54 +0000

    Automatically backed up by Learn

---
+++ tictactoe.rb
@@ -0,0 +1,14 @@
+row = [" ", " ", " "]
+ 
+# Position 1: Left
+row[0]
+ 
+# Position 2: Middle
+row[1]
+ 
+# Position 3: Right
+row[2]
+ 
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
