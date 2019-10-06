3d5095c0cc48858e92bd6804226878a1411415a8
Callum Walsh <makomirocket@gmail.com>
2019-10-06 06:59:40 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,15 @@
 # Define the variable board below.
-defi row = [" ", " ", " "]
+board = row = [" ", " ", " "]row = [" ", " ", " "]
+ 
+# Position 1: Left
+row[0]
+ 
+# Position 2: Middle
+row[1]
+ 
+# Position 3: Right
+row[2]
+ 
+# Move X to the Position 2, Middle
+row[1] = "X"
+row #=> [" ", "X", " "]
